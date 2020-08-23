1f2d4ee03b48bbcfacf4de39896dfb599e35c0de
atatum718 <54254894+atatum718@users.noreply.github.com>
2020-08-23 19:20:15 -0400

    Create classes.js

---
+++ classes.js
@@ -0,0 +1,149 @@
+class Plant {
+  constructor(
+    type,
+    isPerennial,
+    leafDescription,
+    leafColor,
+    flowerColor,
+    flowerDescription,
+    gallonsWaterPerWeek,
+    amountOfSunNeeded
+  ) {
+    this.type = type;
+    this.isPerennial = isPerennial;
+    this.leafDescription = leafDescription;
+    this.leafColor = leafColor;
+    this.flowerColor = flowerColor;
+    this.flowerDescription = flowerDescription;
+    this.gallonsWaterPerWeek = gallonsWaterPerWeek;
+    this.amountOfSunNeeded = amountOfSunNeeded;
+  }
+
+  // This function is already done for you.
+  // The changes are all highlighted in bold
+  // This is the same as the describePlant(plant) function
+  describe() {
+    let description = `A ${this.type} which has ${this.leafColor} leaves ${this.leafDescription}.  The flowers are ${this.flowerColor} ${this.flowerDescription} `;
+    return description;
+  }
+  clone() {
+    return new Plant(
+      this.type,
+      this.isPerennial,
+      this.leafDescription,
+      this.leafColor,
+      this.flowerColor,
+      this.flowerDescription,
+      this.gallonsWaterPerWeek,
+      this.amountOfSunNeeded
+    );
+  }
+}
+
+class garden {
+  constructor(name, listOfPlants) {
+    this.name = name;
+    this.listOfPlants = listOfPlants;
+  }
+  describe() {
+    let description = `The ${this.name} has ${this.listOfPlants.length} plants in it.It contains: A`;
+    for (let plant of this.listOfPlants) {
+      description += plant.describe();
+    }
+    return description;
+  }
+}
+
+class estate {
+  constructor() {
+    this.roseArbor = new garden("roseArbor", []);
+    this.perennialGarden = new garden("perennialGarden", []);
+    this.slopePlanters = new garden("slopePlanters", []);
+  }
+  addPlant(plant) {
+    if (plant.type === "rose") {
+      this.roseArbor.listOfPlants.push(plant);
+    } else if (plant.isPerennial === true && plant.amountOfSunNeeded <= 5) {
+      this.perennialGarden.listOfPlants.push(plant);
+    } else {
+      this.slopePlanters.listOfPlants.push(plant);
+    }
+    return estate;
+  }
+  describe() {
+    let description = "The estate has 3 gardens, they are:";
+    for (let gardenKey in this) {
+      let garden = this[gardenKey];
+      description += garden.describe();
+    }
+    return description;
+  }
+  calculateWaterUsagePerWeek() {
+    let numGallons = 0;
+    // Your Code Here!
+    for (let gardenKey in this) {
+      let garden = this[gardenKey];
+      for (let plant of garden.listOfPlants) {
+        numGallons += plant.gallonsWaterPerWeek;
+      }
+    }
+    return Math.floor(numGallons);
+  }
+
+  cloneAllTheRoses() {
+    let clonedRoses = [];
+    for (let rose of this.roseArbor.listOfPlants) {
+      let clonedRose = rose.clone();
+      clonedRoses.push(clonedRose);
+    }
+    this.roseArbor.listOfPlants = this.roseArbor.listOfPlants.concat(
+      clonedRoses
+    );
+  }
+}
+
+// tests ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------>
+let myEstate = new estate();
+
+let firstPlant = new Plant(
+  "rose",
+  true,
+  "rounded with a point",
+  "green",
+  "red",
+  "concentric circles of pedals",
+  0.8,
+  4
+);
+myEstate.addPlant(firstPlant);
+
+let secondPlant = new Plant(
+  "orchid",
+  true,
+  "long and wide with a point at the end",
+  "green",
+  "fuscia",
+  "pedals surrounding a central mouth",
+  1.2,
+  2
+);
+myEstate.addPlant(secondPlant);
+
+let thirdPlant = new Plant(
+  "marigold",
+  false,
+  "thin and jagged along branches",
+  "green",
+  "yellow and orange",
+  "rounded pedals in groups of five with a darker orange center",
+  0.8,
+  4
+);
+myEstate.addPlant(thirdPlant);
+
+myEstate.describe(); // This should print the whole description of the estate.
+
+myEstate.calculateWaterUsagePerWeek(); // This should print 2.8
+
+myEstate.cloneAllTheRoses(); // This should clone the rose and make a second one.
+console.log(myEstate.roseArbor.listOfPlants.length == 2);
